0f24b62d31b03b10ab24e52c2db3f425f4d3e82e
Ben B <benbasuni1@gmail.com>
2018-04-29 23:51:44 -0700

    added comments to 01 and 02

--- src/individual/01-identity-first-last.js
+++ src/individual/01-identity-first-last.js
@@ -1,13 +1,20 @@
+// Return self
 const identity = val => val;
 
 const first = (arr, n) => {
+
+  // If N is undefined return 1st element
   if (n === undefined) return arr[0];
 
+  // Otherwise, return first elem to N
   return arr.slice(0, n);
 }
 
 const last = (arr, n) => {
+
+  // If N is undefined return last element
   if (n === undefined) return arr[arr.length - 1];
 
+  // Otherwise, return N to last length of arr (0 if arr.length - n is negative)
   return arr.slice(Math.max((arr.length - n), 0), arr.length);
 }
--- src/individual/02-each-indexOf-filter-reject.js
+++ src/individual/02-each-indexOf-filter-reject.js
@@ -1,18 +1,32 @@
 const each = (collection, iterator) => {
+
+  // If Array
   if (Array.isArray(collection)) {
+
+    // Loop through arr elements, and perform iteration as val, key, col
     for (let i = 0; i < collection.length; i++)
       iterator(collection[i], i, collection);
+
+  // If Object
   } else {
+
+    // Loop through obj elements, and perform iteration as val, key, col
     for (let i in collection)
       iterator(collection[i], i, collection);
   }
 }
 
 const indexOf = (arr, target) => {
+
+  // Set result to -1
   let result = -1;
 
+  // Go through each item in array
   each(arr, (item, index) => {
+
+    // If item === target and result is -1 (reason for result = -1 is so that it takes only the 1st instance)
     if (item === target && result === -1)
+      // result becomes index
       result = index;
   });
 
@@ -22,7 +36,10 @@ const indexOf = (arr, target) => {
 const filter = (collection, test) => {
   let result = [];
 
+  // Loop through all items in collection
   each(collection, elem => {
+
+    // If the item passes the test, push it onto results
     if (test(elem)) results.push(elem);
   });
 
@@ -30,7 +47,11 @@ const filter = (collection, test) => {
 }
 
 const reject = (collection, test) => {
+
+  // Return the filtering through the collection
   return filter(collection, elem => {
+
+    // If item does not pass the test, return the elem (let it get pushed ont what is being returned)
     if (!test(elem)) return elem;
   });
 };
